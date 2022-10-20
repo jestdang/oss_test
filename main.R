a596a6fc7efc24b83ff4d539eab417445cd06261
Jessica Dang <91989319+jestdang@users.noreply.github.com>
2022-10-20 18:44:28 -0400

    Update main.R

--- main.R
+++ main.R
@@ -1,1 +1,17 @@
+# Fibonacci function using a for loop
+
+loopFib = function(n) {
+  
+  # starts the sequence with: 1, 1
+  fibSeq = c(1, 1)
+  
+  if (n > 2) {
+    for (i in 3:n) {
+      fibSeq[i] = fibSeq[i - 1] + fibSeq[i - 2]
+    }
+  }
+  
+  # return the nth number in the sequence
+  fibSeq[n]
+}
 
